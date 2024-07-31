6a896926baa6ddd782ffd89b546c843453e4a771
abhinav2210103 <abhinav2210103@akgec.ac.in>
2024-07-31 14:46:07 +0530

    Integerated Blogs get Api

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, ActivityIndicator } from 'react-native';
+import { View, Text, Image, ActivityIndicator, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import axios from 'axios';
 import { EXPO_APP_BASE_URL } from '@env';
 
 export default function HomeScreen() {
   const [topic, setTopic] = useState(null);
+  const [blogs, setBlogs] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -14,6 +15,8 @@ export default function HomeScreen() {
       try {
         const response = await axios.get(`${EXPO_APP_BASE_URL}/topic/get`);
         setTopic(response.data);
+        const blogsResponse = await axios.get(`${EXPO_APP_BASE_URL}/blog/all`);
+        setBlogs(blogsResponse.data.blogs);
       } catch (err) {
         console.error('Error fetching current topic:', err);
         setError('Failed to fetch topic');
@@ -44,7 +47,7 @@ export default function HomeScreen() {
   return (
     <SafeAreaView className='p-5'>
       <View className='mb-3'>
-        <Text className='text-3xl font-bold'> Welcome</Text>
+        <Text className='text-3xl font-bold'>Welcome</Text>
       </View>
       <View>
         <Text className='text-xl'>Hey, Today's Topic is:</Text>
@@ -54,13 +57,28 @@ export default function HomeScreen() {
             {topic.imageUrl && (
               <Image
                 source={{ uri: topic.imageUrl }}
-                style={{ width: '100%', height: 200, marginTop: 10, resizeMode: 'cover'}}
+                style={{ width: '100%', height: 200, marginTop: 10, resizeMode: 'cover' }}
               />
             )}
           </>
         ) : (
           <Text>No topic available</Text>
         )}
+      </View>
+      <View className='mt-5'>
+        <Text className='text-xl font-bold'>Recent Blogs:</Text>
+        <ScrollView>
+          {blogs.length > 0 ? (
+            blogs.map((blog) => (
+              <View key={blog._id} className='border-b border-gray-300 p-2 mb-3'>
+                <Text className='font-bold'>{blog.createdBy.fullName}</Text>
+                <Text className='text-gray-600'>{blog.thoughts}</Text>
+              </View>
+            ))
+          ) : (
+            <Text>No comments available</Text>
+          )}
+        </ScrollView>
       </View>
     </SafeAreaView>
   );

06059c2b98a69b8a1400503b352e1a3e8868a2ac
abhinav2210103 <abhinavmishra0493>
2024-08-17 23:44:27 +0530

    Card UI Completed in profile page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,34 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Alert, TextInput, BackHandler } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useRouter } from 'expo-router';
-import { LinearGradient } from 'expo-linear-gradient';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  TextInput,
+  BackHandler,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router";
+import { LinearGradient } from "expo-linear-gradient";
+import axios from "axios";
+import Likes from "../../assets/images/likes.svg";
+import Comments from "../../assets/images/comments.svg";
+import Edit from '../../assets/images/edit.svg';
+import Privacy from '../../assets/images/privacy.svg'
 
 export default function Profile() {
   const router = useRouter();
   const [userProfile, setUserProfile] = useState({});
   const [step, setStep] = useState(0);
-  const [newFullName, setNewFullName] = useState('');
-  const [currentPassword, setCurrentPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
+  const [newFullName, setNewFullName] = useState("");
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
   const handleLogout = async () => {
-    await AsyncStorage.removeItem('token');
-    axios.defaults.headers.common['Authorization'] = '';
-    router.replace('/welcome');
+    await AsyncStorage.removeItem("token");
+    axios.defaults.headers.common["Authorization"] = "";
+    router.replace("/welcome");
   };
 
   useEffect(() => {
     const fetchUserProfile = async () => {
       try {
-        const response = await axios.get(`${process.env.EXPO_PUBLIC_BASE_URL}/user/profile`);
+        const response = await axios.get(
+          `${process.env.EXPO_PUBLIC_BASE_URL}/user/profile`
+        );
         setUserProfile(response.data);
         setNewFullName(response.data.fullName);
       } catch (error) {
-        console.error('Error fetching user profile:', error);
-        Alert.alert('Error', 'Failed to fetch user profile');
+        console.error("Error fetching user profile:", error);
+        Alert.alert("Error", "Failed to fetch user profile");
       }
     };
 
@@ -39,90 +52,186 @@ export default function Profile() {
     const backAction = () => {
       if (step === 1) {
         setStep(0);
-        return true; // Prevent default behavior (going back to the home screen)
+        return true;
       }
-      return false; // Allow default behavior
+      return false;
     };
 
     const backHandler = BackHandler.addEventListener(
-      'hardwareBackPress',
+      "hardwareBackPress",
       backAction
     );
 
-    return () => backHandler.remove(); // Cleanup on component unmount
+    return () => backHandler.remove();
   }, [step]);
 
   const handleSaveChanges = async () => {
     try {
       if (newFullName !== userProfile.fullName) {
-        const response = await axios.post(`${process.env.EXPO_PUBLIC_BASE_URL}/user/resetusername`, { newFullName });
+        const response = await axios.post(
+          `${process.env.EXPO_PUBLIC_BASE_URL}/user/resetusername`,
+          { newFullName }
+        );
         if (response.status === 200) {
           setUserProfile((prev) => ({ ...prev, fullName: newFullName }));
-          Alert.alert('Success', 'Username updated successfully');
+          Alert.alert("Success", "Username updated successfully");
         }
       }
 
       if (newPassword) {
-        const response = await axios.post(`${process.env.EXPO_PUBLIC_BASE_URL}/user/resetpassword`, { currentPassword, newPassword });
+        const response = await axios.post(
+          `${process.env.EXPO_PUBLIC_BASE_URL}/user/resetpassword`,
+          { currentPassword, newPassword }
+        );
         if (response.status === 200) {
-          Alert.alert('Success', 'Password updated successfully');
+          Alert.alert("Success", "Password updated successfully");
         }
       }
       setStep(0);
     } catch (error) {
-      console.error('Error saving changes:', error);
-      Alert.alert('Error', 'Failed to save changes');
+      console.error("Error saving changes:", error);
+      Alert.alert("Error", "Failed to save changes");
     }
   };
 
   return (
-    <SafeAreaView className='flex-1'>
+    <SafeAreaView className="flex-1">
       <LinearGradient
-        colors={['hsla(242, 47%, 13%, 1)', 'hsla(256, 31%, 23%, 1)']}
-        className='flex-1'
+        colors={["hsla(242, 47%, 13%, 1)", "hsla(256, 31%, 23%, 1)"]}
+        className="flex-1"
         start={{ x: 0, y: 1 }}
         end={{ x: 0, y: 0 }}
       >
-        <View className='flex-1 justify-center items-center'>
+        <View className="flex-1 justify-center items-center">
           {step === 0 ? (
             <>
-              <Text className='text-5xl text-white mb-20' style={{ fontFamily: 'baloo-semi' }}>
+              <Text
+                className="text-5xl text-white mb-20 pt-4"
+                style={{ fontFamily: "baloo-semi" }}
+              >
                 Profile
               </Text>
 
-              <View className='w-[80%] grid grid-cols-2 gap-4'>
-                <View className='bg-[#8B4513] rounded-2xl h-20 justify-center items-center'>
-                  <Text className='text-white text-lg' style={{ fontFamily: 'nunito-bold' }}>
-                    Total Likes: {userProfile.totalLikes}
-                  </Text>
-                </View>
-
-                <View className='bg-[#4682B4] rounded-2xl h-20 justify-center items-center'>
-                  <Text className='text-white text-lg' style={{ fontFamily: 'nunito-bold' }}>
-                    Total Blogs: {userProfile.totalBlogs}
-                  </Text>
-                </View>
-
-                <TouchableOpacity onPress={() => setStep(1)} className='bg-[#228B22] rounded-2xl h-20 justify-center items-center'>
-                  <Text className='text-white text-lg' style={{ fontFamily: 'nunito-bold' }}>
-                    Edit Profile
-                  </Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity className='bg-[#CD5C5C] rounded-2xl h-20 justify-center items-center'>
-                  <Text className='text-white text-lg' style={{ fontFamily: 'nunito-bold' }}>
-                    Privacy Policy
-                  </Text>
-                </TouchableOpacity>
+              <View className="w-[90%] flex flex-row gap-6  items-center justify-center">
+                <LinearGradient
+                  colors={[
+                    "rgba(255, 255, 255, 0.1)",
+                    "rgba(153, 153, 153, 0.1)",
+                  ]}
+                  start={{ x: 0, y: 0 }}
+                  end={{ x: 1, y: 1 }}
+                  className="rounded-2xl w-[45%]"
+                >
+                  <View className="h-36 justify-center items-center">
+                    <Likes />
+                    <View>
+                      <Text
+                        className="text-white text-lg mt-1"
+                        style={{ fontFamily: "nunito-bold" }}
+                      >
+                        {userProfile.totalLikes}
+                      </Text>
+                    </View>
+                    <View>
+                      <Text
+                        className="text-[#50516D] mt-1 text-lg"
+                        style={{ fontFamily: "nunito-bold" }}
+                      >
+                        Likes
+                      </Text>
+                    </View>
+                  </View>
+                </LinearGradient>
+                <LinearGradient
+                  colors={[
+                    "rgba(255, 255, 255, 0.1)",
+                    "rgba(153, 153, 153, 0.1)",
+                  ]}
+                  start={{ x: 0, y: 0 }}
+                  end={{ x: 1, y: 1 }}
+                  className="rounded-2xl w-[45%]"
+                >
+                  <View className="h-36 justify-center items-center">
+                    <View
+                      style={{
+                        backgroundColor: "rgba(186, 35, 150, 0.1)",
+                        borderRadius: 32,
+                      }}
+                      className="p-3"
+                    >
+                      <Comments />
+                    </View>
+                    <View>
+                      <Text
+                        className="text-white text-lg mt-1"
+                        style={{ fontFamily: "nunito-bold" }}
+                      >
+                        {userProfile.totalBlogs}
+                      </Text>
+                    </View>
+                    <View>
+                      <Text
+                        className="text-[#50516D] mt-1 text-lg"
+                        style={{ fontFamily: "nunito-bold" }}
+                      >
+                        Comments
+                      </Text>
+                    </View>
+                  </View>
+                </LinearGradient>
+              </View>
+              <View className="w-[90%] flex flex-row gap-6  items-center justify-center mt-1">
+                <LinearGradient
+                  colors={[
+                    "rgba(255, 255, 255, 0.1)",
+                    "rgba(153, 153, 153, 0.1)",
+                  ]}
+                  start={{ x: 0, y: 0 }}
+                  end={{ x: 1, y: 1 }}
+                  className="rounded-xl w-[45%]"
+                >
+                  
+                  <TouchableOpacity
+                    onPress={() => setStep(1)}
+                    className="rounded-2xl h-36 justify-center items-center w-full"
+                  >
+                     <Edit/>
+                    <Text
+                      className="text-[#3BC1F8]/50 text-lg mt-2"
+                      style={{ fontFamily: "nunito" }}
+                    >
+                      Edit Profile
+                    </Text>
+                  </TouchableOpacity>
+                </LinearGradient>
+                <LinearGradient
+                  colors={[
+                    "rgba(255, 255, 255, 0.1)",
+                    "rgba(153, 153, 153, 0.1)",
+                  ]}
+                  start={{ x: 0, y: 0 }}
+                  end={{ x: 1, y: 1 }}
+                  className="rounded-xl w-[45%]"
+                >
+                  <TouchableOpacity className="rounded-2xl h-36 justify-center items-center w-full">
+                    <Privacy/>
+                    <Text
+                      className="text-[#F34751]/50 text-lg mt-2"
+                      style={{ fontFamily: "nunito" }}
+                    >
+                      Privacy Policy
+                    </Text>
+                  </TouchableOpacity>
+                </LinearGradient>
               </View>
             </>
           ) : (
-            <View className='w-[80%]'>
+            <View className="w-[80%]">
               <TextInput
                 value={newFullName}
                 onChangeText={setNewFullName}
                 placeholder={userProfile.fullName || "Current Username"}
-                className='bg-white rounded-2xl h-12 mb-4 px-4'
+                className="bg-white rounded-xl h-12 mb-4 px-4"
               />
 
               <TextInput
@@ -130,7 +239,7 @@ export default function Profile() {
                 onChangeText={setCurrentPassword}
                 placeholder="Current Password"
                 secureTextEntry
-                className='bg-white rounded-2xl h-12 mb-4 px-4'
+                className="bg-white rounded-2xl h-12 mb-4 px-4"
               />
 
               <TextInput
@@ -138,11 +247,17 @@ export default function Profile() {
                 onChangeText={setNewPassword}
                 placeholder="New Password"
                 secureTextEntry
-                className='bg-white rounded-2xl h-12 mb-4 px-4'
+                className="bg-white rounded-2xl h-12 mb-4 px-4"
               />
 
-              <TouchableOpacity onPress={handleSaveChanges} className='bg-[#228B22] rounded-2xl h-12 justify-center items-center'>
-                <Text className='text-white text-lg' style={{ fontFamily: 'nunito-bold' }}>
+              <TouchableOpacity
+                onPress={handleSaveChanges}
+                className="bg-[#228B22] rounded-2xl h-12 justify-center items-center"
+              >
+                <Text
+                  className="text-white text-lg"
+                  style={{ fontFamily: "nunito-bold" }}
+                >
                   Save Changes
                 </Text>
               </TouchableOpacity>
@@ -150,8 +265,16 @@ export default function Profile() {
           )}
 
           {step === 0 && (
-            <TouchableOpacity onPress={handleLogout} className='mt-10 bg-red-500 rounded-2xl h-12 w-[80%] justify-center items-center'>
-              <Text className='text-white text-lg' style={{ fontFamily: 'mulish-black' }}>Logout</Text>
+            <TouchableOpacity
+              onPress={handleLogout}
+              className="mt-5 bg-red-500 rounded-2xl h-12 w-[80%] justify-center items-center"
+            >
+              <Text
+                className="text-white text-lg"
+                style={{ fontFamily: "mulish-black" }}
+              >
+                Logout
+              </Text>
             </TouchableOpacity>
           )}
         </View>

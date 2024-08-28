d16d5df92c0dcc1a6a131e0dfb1be3805857ecea
abhinav2210103 <abhinavmishra0493@gmail.com>
2024-08-28 22:41:30 +0530

    Integrate previous topics API in history

--- app/(tabs)/history.jsx
+++ app/(tabs)/history.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text,SafeAreaView } from 'react-native'
 
 export default function history() {
+  const [topics, setTopics] = useState([]);
+  useEffect(() => {
+    const fetchAllTopics = async () => {
+      try {
+        const response = await axios.get( `${process.env.EXPO_PUBLIC_BASE_URL}/topic/all`, {
+          withCredentials: true,
+        });
+        setTopics(response.data);
+        console.log(response.data);
+      } catch (err) {
+        console.error('Error fetching current topic:', err);
+        setError('Failed to fetch topic');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchAllTopics();
+  }, []);
+
   return (
+ <SafeAreaView>
     <View>
-      <Text>history</Text>
+      <Text>histohujjnjnjnjnjnry</Text>
     </View>
+    </SafeAreaView>
   )
 }

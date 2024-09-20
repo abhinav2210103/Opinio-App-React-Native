93b38e642014c5c65f2130f1c5072cbfae361e15
Abhinav Mishra <abhinavmishra0493@gmail.com>
2024-09-20 19:51:00 +0530

    Refactor SVG logo files and update home component UI

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, SafeAreaView, ScrollView } from 'react-native';
-import axios from 'axios';
-import Blogs from '../../components/Blogs';
-import Topic from '../../components/Topic';
+import React, { useEffect, useState } from "react";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import axios from "axios";
+import Blogs from "../../components/Blogs";
+import Topic from "../../components/Topic";
 import AppLoader from "../../components/AppLoader";
-import { LinearGradient } from 'expo-linear-gradient';
-import Line from '../../assets/images/line.svg'; 
+import { LinearGradient } from "expo-linear-gradient";
+import Line from "../../assets/images/line.svg";
+import Logoinner from "../../assets/images/logoinner.svg";
+import LogoOuter from "../../assets/images/logoouter.svg";
 
 export default function HomeScreen() {
   const [topic, setTopic] = useState(null);
@@ -16,17 +18,23 @@ export default function HomeScreen() {
   useEffect(() => {
     const fetchCurrentTopic = async () => {
       try {
-        const response = await axios.get(`${process.env.EXPO_PUBLIC_BASE_URL}/topic/get`, {
-          withCredentials: true,
-        });
+        const response = await axios.get(
+          `${process.env.EXPO_PUBLIC_BASE_URL}/topic/get`,
+          {
+            withCredentials: true,
+          }
+        );
         setTopic(response.data);
-        const blogsResponse = await axios.get(`${process.env.EXPO_PUBLIC_BASE_URL}/blog/all`, {
-          withCredentials: true,
-        });
+        const blogsResponse = await axios.get(
+          `${process.env.EXPO_PUBLIC_BASE_URL}/blog/all`,
+          {
+            withCredentials: true,
+          }
+        );
         setBlogs(blogsResponse.data.blogs);
       } catch (err) {
-        console.error('Error fetching current topic:', err);
-        setError('Failed to fetch topic');
+        console.error("Error fetching current topic:", err);
+        setError("Failed to fetch topic");
       } finally {
         setLoading(false);
       }
@@ -49,22 +57,32 @@ export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1">
       <LinearGradient
-        colors={['hsla(242, 47%, 13%, 1)', 'hsla(256, 31%, 23%, 1)']}
+        colors={["hsla(242, 47%, 13%, 1)", "hsla(256, 31%, 23%, 1)"]}
         className="flex-1"
         start={{ x: 0, y: 1 }}
         end={{ x: 0, y: 0 }}
       >
-        <View>
-          <View className='mb-3'>
-            <Text className='text-3xl font-bold text-white'>Welcome</Text>
+        <View className='mt-4'>
+          <View className="flex items-center justify-center flex-row gap-2 mt-2">
+            <View className="relative w-12 h-12 flex items-center justify-center mt-4">
+              <LogoOuter width={40} height={40} />
+              <View className="absolute">
+                <Logoinner width={25} height={25} />
+              </View>
+            </View>
+            <View>
+            <Text className="text-3xl font-bold text-white" style={{ fontFamily: 'baloo-semi' }}>
+              Today’s Topic
+            </Text>
+            </View>
           </View>
-          <Topic topic={topic}/>
+          <Topic topic={topic} />
           <ScrollView>
             <View className="bg-[#2F2753] mx-1 flex rounded-3xl">
-              <View className='flex justify-center items-center mt-5'>
-              <Line/>
+              <View className="flex justify-center items-center mt-5">
+                <Line />
               </View>
-              <Blogs blogs={blogs}/>
+              <Blogs blogs={blogs} />
             </View>
           </ScrollView>
         </View>
